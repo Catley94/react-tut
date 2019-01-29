7a0152562200b6d003f18fb95e71b78cc0fac3b7
Samuel Catley <scp94373@gmail.com>
2019-01-29 20:20:43 +0000

    Update1
    Update1

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+// We have not defined the handleClick() method yet, so our code crashes.
 class Square extends React.Component {
+
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button
+      className="square"
+      onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
   renderSquare(i) {
-    return <Square />;
+    return <Square
+    value={this.state.squares[i]}
+    onClick={() => this.handleClick(i)} />;
   }
 
   render() {
